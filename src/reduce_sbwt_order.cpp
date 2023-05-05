191e7a1659e5c3cd1925745a7648eeb8642b50d0
Jarno N. Alanko <alanko.jarno@gmail.com>
2023-05-05 12:18:22 +0300

    Reduce order from a list of orders

--- src/reduce_sbwt_order.cpp
+++ src/reduce_sbwt_order.cpp
@@ -13,16 +13,19 @@ using namespace sbwt;
 int main(int argc, char** argv){
 
     if(argc == 1){
-        cerr << "Please give a plain-matrix sbwt file" << endl;
+        cerr << "Usage: " << argv[0] << " index.sbwt output_prefix 16 32 48 64 80 96 112 128 144 160 176 192 208 224 240 255" << endl;
         return 1;
     }
 
     string sbwt_index_file = string(argv[1]);
-    string out_file = string(argv[2]);
-    int64_t new_k = stoll(argv[3]);
+    string out_prefix = string(argv[2]);
+    vector<int64_t> new_k_values;
+    for(int64_t i = 3; i < argc; i++){
+        int64_t k = stoll(argv[i]);
+        new_k_values.push_back(k);
+    }
 
     sbwt::throwing_ifstream in(sbwt_index_file, ios::binary);
-    sbwt::throwing_ofstream out(out_file, ios::binary);
     string variant = load_string(in.stream); // read variant type
     if(variant != "plain-matrix"){
         cerr << "Error: input is not a plain-matrix SBWT" << endl;
@@ -33,15 +36,24 @@ int main(int argc, char** argv){
     sbwt.load(in.stream);
     cerr << "Loaded a plain matrix SBWT with " << sbwt.number_of_subsets() << " subsets" << endl;
 
+    int64_t max_k = *std::max_element(new_k_values.begin(), new_k_values.end());
+    if(max_k > sbwt.get_k()){
+        cerr << "Error: new k must be smaller or equal to the original. New = " << max_k << ", original = " << sbwt.get_k() << endl;
+        return 1;
+    }
+
     cerr << "Building the LCS" << endl;
     sdsl::int_vector lcs = lcs_linear_algorithm(sbwt);
 
-    cerr << "Reducing SBWT order" << endl;
-    sbwt::plain_matrix_sbwt_t new_sbwt = reduce_sbwt_order(sbwt, lcs, new_k);
+    for(int64_t k : new_k_values){
+        cerr << "Reducing to order " << k << endl;
+        sbwt::throwing_ofstream out_file(out_prefix + "-" + to_string(k) + ".sbwt", ios::binary);
+        
+        sbwt::plain_matrix_sbwt_t new_sbwt = reduce_sbwt_order(sbwt, lcs, k);
 
-    cerr << "Reduced SBWT has " << new_sbwt.number_of_subsets() << " subsets" << endl;
+        cerr << "SBWT of order " << k << " has " << new_sbwt.number_of_subsets() << " subsets" << endl;
 
-    serialize_string("plain-matrix", out.stream);
-    new_sbwt.serialize(out.stream);
-
+        serialize_string("plain-matrix", out_file.stream);
+        new_sbwt.serialize(out_file.stream);
+    }
 }
