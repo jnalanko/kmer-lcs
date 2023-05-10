69e33403a781b60c48668ca5f55c929145b1b1cf
Jarno N. Alanko <alanko.jarno@gmail.com>
2023-05-10 23:01:20 +0300

    Parallel speedup plot

---
+++ parallel_speedup_plot.py
@@ -0,0 +1,20 @@
+import sys
+import matplotlib.pyplot as plt
+
+def parse(filename):
+    times = []
+    for line in open(filename):
+        tokens = line.split(',')
+        times.append(float(tokens[1]))
+    return times
+
+times = parse(sys.argv[1])
+threads = list(range(1, len(times) + 1))
+
+speedups = [times[0] / t for t in times]
+plt.plot(threads, speedups)
+
+# Plot dashed line at y = x
+plt.plot(threads, threads, linestyle='--', color='black')
+
+plt.show()
