784931fb8efe6fa4ebbee1155cc7a69dd29fa72e
Jarno N. Alanko <alanko.jarno@gmail.com>
2023-05-10 21:44:52 +0300

    Add basic parallel

--- include/lcs_basic_parallel_algorithm.hpp
+++ include/lcs_basic_parallel_algorithm.hpp
@@ -75,7 +75,7 @@ sdsl::int_vector<> lcs_basic_parallel_algorithm(const sbwt::plain_matrix_sbwt_t&
             const sdsl::bit_vector* bits[4] = {&A_bits, &C_bits, &G_bits, &T_bits};
             lcs_worker_thread(ptrs, last.data(), bits, start, end);
         }
-        
+
         last = propagated;
     }
 
--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -8,6 +8,7 @@
 
 #include "lcs_naive_algorithm.hpp"
 #include "lcs_basic_algorithm.hpp"
+#include "lcs_basic_parallel_algorithm.hpp"
 #include "lcs_linear_algorithm.hpp"
 #include "lcs_superalphabet_algorithm.hpp"
 
@@ -40,8 +41,9 @@ int main(int argc, char** argv){
     if(argc == 1){
         cerr << "Usage: " << argv[0] << " <sbwt-file> <algorithm> <output-file>" << endl;
         cerr << "The sbwt file must be in plain-matrix format." << endl;
-        cerr << "The algorithm is one of: naive, basic, superalphabet-x, linear," << endl;
-        cerr << "where the x in superalphabet-x is a number specifying the number of characters in each supercharacter" << endl;
+        cerr << "The algorithm is one of: naive, basic, basic-parallel-x superalphabet-x, linear," << endl;
+        cerr << "where the x in superalphabet-x is the number of characters in each supercharacter," << endl;
+        cerr << "and the x in basic-parallel-x is the number of threads." << endl;
         cerr << "Appends one line to the output file containing the result," << endl;
         return 1;
     }
@@ -66,6 +68,13 @@ int main(int argc, char** argv){
         run_with_timing(sbwt, lcs_naive_algorithm, "Naive", out.stream);
     } else if(algorithm == "basic"){
         run_with_timing(sbwt, lcs_basic_algorithm, "Basic", out.stream);
+    } else if(algorithm.substr(0, 15) == "basic-parallel-"){
+        int64_t num_threads = stoll(algorithm.substr(15));
+        auto basic_parallel_algorithm = [num_threads](const sbwt::plain_matrix_sbwt_t& sbwt){
+            return lcs_basic_parallel_algorithm(sbwt, num_threads);
+        };
+        run_with_timing(sbwt, basic_parallel_algorithm, "Basic-Parallel-" + to_string(num_threads), out.stream);
+        
     } else if(algorithm == "linear"){
         run_with_timing(sbwt, lcs_linear_algorithm, "Linear", out.stream);
     } else if(algorithm.substr(0, 14) == "superalphabet-"){
