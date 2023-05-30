1acdea86a501c5ec132ec2769edffacafc90c9be
Jarno N. Alanko <alanko.jarno@gmail.com>
2023-05-30 07:06:38 -0700

    K-mer plot and flip x and y in tradeoff plot

--- plot.py
+++ plot.py
@@ -56,14 +56,50 @@ def plot_human(linear_infile, basic_infile, dataset_name, outfile_prefix):
                     "Memory ({})".format(dataset_name), 
                     "{}_mem_by_k.pdf".format(outfile_prefix))
     
-    do_scatter_plot(linear_time, linear_mem, basic_time, basic_mem, 
-                    "Linear", "Basic", "Time (s)", "Memory (GB)", 
-                    "Time and space ({})".format(dataset_name), 
-                    "{}_time_mem_tradeoff.pdf".format(outfile_prefix))
-    
+    do_scatter_plot(basic_time, basic_mem, linear_time, linear_mem, 
+                    "Linear", "Basic", "Memory (GB)", "Time (s)", 
+                    "Space and time ({})".format(dataset_name), 
+                    "{}_mem_time_tradeoff.pdf".format(outfile_prefix))
+
+def read_kmer_curve(filename):
+    K, SIZE = [], [] 
+    for line in open(filename):
+        tokens = line.split(",")
+        k, size = int(tokens[-2]), int(tokens[-1])
+        K.append(k)
+        SIZE.append(size)
+    return K, SIZE
+
+def make_kmer_plot(human_file, coli_file, metagenome_file, outfile):
+
+    human_K, human_SIZE = read_kmer_curve(human_file)
+    coli_K, coli_SIZE = read_kmer_curve(coli_file)
+    metagenome_K, metagenome_SIZE = read_kmer_curve(metagenome_file)
+
+    plt.figure()
+
+    plt.plot(human_K, human_SIZE, label="Human genome", marker="x")
+    plt.plot(coli_K, coli_SIZE, label="E. coli genomes", marker="x")
+    plt.plot(metagenome_K, metagenome_SIZE, label="Metagenome reads", marker="x")
+
+    plt.xlabel("k")
+    plt.ylabel("n")
+
+    # Start axes from origin
+    plt.xlim(xmin=0)
+    plt.ylim(ymin=0)
+
+    plt.legend()
+    plt.title("Number of sets in SBWT")
+
+    print("Saving to", outfile)
+    plt.savefig(outfile)
+
+
 if not os.path.exists("plots"):
    os.makedirs("plots")
 
+make_kmer_plot("data_for_plots/linear_human.csv", "data_for_plots/linear_coli.csv", "data_for_plots/linear_metagenome.csv", "plots/kmers.pdf")
 plot_human("data_for_plots/linear_human.csv", "data_for_plots/basic_human.csv", "Human genome", "plots/human")
 plot_human("data_for_plots/linear_coli.csv", "data_for_plots/basic_coli.csv", "E. coli genomes", "plots/coli")
 plot_human("data_for_plots/linear_metagenome.csv", "data_for_plots/basic_metagenome.csv", "Metagenome reads", "plots/metagenome")
