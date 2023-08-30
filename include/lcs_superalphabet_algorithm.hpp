59e2e7dcff30c841b507ccc90712d0ccb2bc7ff6
ElenaBiagi <elena.biagi24@gmail.com>
2023-08-30 11:38:39 +0300

    From uint64_t to char

--- include/lcs_superalphabet_algorithm.hpp
+++ include/lcs_superalphabet_algorithm.hpp
@@ -4,7 +4,7 @@
 #include <cmath>
 #include <unordered_map>
 
-uint64_t get_keyab_from_key(uint64_t key, const std::unordered_map<uint64_t,uint64_t>& alphabet_map) {
+char get_keyab_from_key(char key, const std::unordered_map<char,char>& alphabet_map) {
     auto it = alphabet_map.find(key);
     if (it != alphabet_map.end()) {
         return it->second;
@@ -13,20 +13,20 @@ uint64_t get_keyab_from_key(uint64_t key, const std::unordered_map<uint64_t,uint
 }
 
 struct alphabet_str{
-    vector<uint64_t> super_alphabet;
-    unordered_map<uint64_t,uint64_t> super_alphabet_map;
+    vector<char> super_alphabet;
+    unordered_map<char,char> super_alphabet_map;
 };
 
-alphabet_str get_super_alphabet(const vector<uint64_t>& alphabet, const char base){ // base = base of the original alphabet
+alphabet_str get_super_alphabet(const vector<char>& alphabet, const char base){ // base = base of the original alphabet
     alphabet_str result;
-    uint64_t excluded_v = (1ULL << (static_cast<uint64_t>(pow(3, base/2))-1));
-    vector<uint64_t> super_alphabet;
-    unordered_map<uint64_t, uint64_t> super_alphabet_map;
+    char excluded_v = (1ULL << (static_cast<char>(pow(3, base/2))-1));
+    vector<char> super_alphabet;
+    unordered_map<char, char> super_alphabet_map;
     super_alphabet_map.reserve(alphabet.size()*(alphabet.size()-1)+1);
     //remove chars w $
-    for(uint64_t c2:alphabet){
-        for (uint64_t c1:alphabet){
-            uint64_t  super_char = ( c1 << (3 * base)) | c2;
+    for(char c2:alphabet){
+        for (char c1:alphabet){
+            char  super_char = ( c1 << (3 * base)) | c2;
             super_alphabet_map[super_char] = super_alphabet.size();
             super_alphabet.push_back(super_char);
             if (super_char < excluded_v){break;} // avoid char ending with $
@@ -37,19 +37,19 @@ alphabet_str get_super_alphabet(const vector<uint64_t>& alphabet, const char bas
     return result;
 }
 
-bool compare_2_char(uint64_t compare_res){
+bool compare_2_char(char compare_res){
     compare_res = compare_res & 0b111;
     return compare_res == 0;
 }
 
 struct concat_str{
-    vector<uint64_t> concat_v;
+    vector<char> concat_v;
     sdsl::bit_vector concat_b;
 };
 
-concat_str get_super_concat(char base, const vector<uint64_t>& concat_vector, const sdsl::bit_vector& concat_bits, const vector<uint64_t>& C_array, const vector<uint64_t>& super_alphabet,const unordered_map<uint64_t,uint64_t>& super_alphabet_map, const vector<uint64_t>& alphabet){
+concat_str get_super_concat(char base, const vector<char>& concat_vector, const sdsl::bit_vector& concat_bits, const vector<uint64_t>& C_array, const vector<char>& super_alphabet,const unordered_map<char,char>& super_alphabet_map, const vector<char>& alphabet){
     concat_str result;
-    vector<uint64_t> res_concat_v;
+    vector<char> res_concat_v;
     vector<bool> bv;
 
     // Scan concat_bv once and add a pointer for every element in the alphabet
@@ -80,7 +80,7 @@ concat_str get_super_concat(char base, const vector<uint64_t>& concat_vector, co
                 continue; }
         }
         // Find the start of the curr_char-block and append to curr_char the values found in the subset at the start of its block
-        uint64_t curr_char = concat_vector[x]; // C1 => curr_char + c2
+        char curr_char = concat_vector[x]; // C1 => curr_char + c2
         C_copy[curr_char]++;
         uint64_t x_subset = C_copy[curr_char];
 
@@ -92,8 +92,8 @@ concat_str get_super_concat(char base, const vector<uint64_t>& concat_vector, co
         // Scan the subset and add each character found to curr_char
         // (2) Check you are not appending an empty set
         while (x_subset_start_bv < concat_bits.size() && !concat_bits[x_subset_start_bv]){
-            uint64_t key = (alphabet[curr_char] << (3*(base/2)))| alphabet[concat_vector[x_subset_start_v]] ;
-            uint64_t key_ab = get_keyab_from_key( key, super_alphabet_map);
+            char key = (alphabet[curr_char] << (3*(base/2)))| alphabet[concat_vector[x_subset_start_v]] ;
+            char key_ab = get_keyab_from_key( key, super_alphabet_map);
 
             res_concat_v.push_back(key_ab);
             x_subset_start_bv ++;
@@ -115,6 +115,7 @@ concat_str get_super_concat(char base, const vector<uint64_t>& concat_vector, co
 
 
 sdsl::int_vector<> lcs_superalphabet_algorithm(const sbwt::plain_matrix_sbwt_t& SBWT, char chars_per_super_alpha_char){
+    cerr << "Starting Superalphabet" << endl;
     char t = 2;
     const sdsl::bit_vector& A_bits = SBWT.get_subset_rank_structure().A_bits;
     const sdsl::bit_vector& C_bits = SBWT.get_subset_rank_structure().C_bits;
@@ -124,7 +125,7 @@ sdsl::int_vector<> lcs_superalphabet_algorithm(const sbwt::plain_matrix_sbwt_t& 
     uint64_t n_nodes = SBWT.number_of_subsets();
     vector<uint64_t> C_array(4);
 
-    vector<uint64_t> last = {0};
+    vector<char> last = {0};
     C_array[0] = last.size();
     for(uint64_t i = 0; i < n_nodes; i++) if(A_bits[i]) last.push_back(1);
     C_array[1] = last.size();
@@ -142,7 +143,7 @@ sdsl::int_vector<> lcs_superalphabet_algorithm(const sbwt::plain_matrix_sbwt_t& 
     sdsl::int_vector<> lcs(n_nodes, 0, 64 - __builtin_clzll(k-1)); // Enough bits per element to store values from 0 to k-1
 
     // Propagate the labels one step forward in the graph
-    vector<uint64_t> propagated(n_nodes, 0);
+    vector<char> propagated(n_nodes, 0);
     uint64_t A_ptr = C_array[0];
     uint64_t C_ptr = C_array[1];
     uint64_t G_ptr = C_array[2];
@@ -163,7 +164,7 @@ sdsl::int_vector<> lcs_superalphabet_algorithm(const sbwt::plain_matrix_sbwt_t& 
 
     // CONCAT with the basic alphabet
     // we already have the C array
-    vector<uint64_t> concat_v;
+    vector<char> concat_v;
     vector<bool> bv;
     for(uint64_t i = 0; i < n_nodes; i++){
         if(A_bits[i]) { concat_v.push_back(1);}
@@ -181,7 +182,7 @@ sdsl::int_vector<> lcs_superalphabet_algorithm(const sbwt::plain_matrix_sbwt_t& 
     }
 
 
-    vector<uint64_t> alphabet = {0,1,2,3,4};//$,A,C,G,T
+    vector<char> alphabet = {0,1,2,3,4};//$,A,C,G,T
 
     // Create a super alphabet
     auto[super_2_alphabet, super_2_alphabet_map] = get_super_alphabet(alphabet, t/2);
@@ -190,9 +191,9 @@ sdsl::int_vector<> lcs_superalphabet_algorithm(const sbwt::plain_matrix_sbwt_t& 
     vector<uint64_t> super_2_C_array(super_2_alphabet.size(), 0);
     for ( size_t i = 1; i < n_nodes; i++ ) { // start from 1 as later i-1 and $$ already ok
         // mismatch in the 1st char
-        uint64_t key = (propagated[i] << 3) | last[i];
+        char key = (propagated[i] << 3) | last[i];
         if (mismatch_found_marks[i]){
-            uint64_t c = super_2_alphabet_map[key];
+            char c = super_2_alphabet_map[key];
             while(super_2_C_array[c]==0){
                 super_2_C_array[c]=i;
                 c--;
@@ -201,7 +202,7 @@ sdsl::int_vector<> lcs_superalphabet_algorithm(const sbwt::plain_matrix_sbwt_t& 
         } else if (!mismatch_found_marks[i] && (propagated[i] ^ propagated[i-1])!=0){
             mismatch_found_marks[i] = 1;
             lcs[i] = 1;
-            uint64_t c = super_2_alphabet_map[key];
+            char c = super_2_alphabet_map[key];
             while(super_2_C_array[c]==0){
                 super_2_C_array[c]=i;
                 c--;
@@ -224,6 +225,8 @@ sdsl::int_vector<> lcs_superalphabet_algorithm(const sbwt::plain_matrix_sbwt_t& 
 
     for (uint64_t round = t; round < (k+t-1); round += t) { // works w chars_per_super_alpha_char = 2
         // Propagate the labels one step forward in the graph
+        cerr << "Round " << to_string(round) << endl;
+
         vector <uint64_t> super_C_copy = {0};
         super_C_copy.insert(super_C_copy.end(), super_2_C_array.begin(), super_2_C_array.end());
         uint64_t subsets = 0;
@@ -242,10 +245,10 @@ sdsl::int_vector<> lcs_superalphabet_algorithm(const sbwt::plain_matrix_sbwt_t& 
             break;}
         for (uint64_t j = 1; j < n_nodes; j++) {
             if (!mismatch_found_marks[j]) {
-                uint64_t pi = super_2_alphabet[propagated[j]];
-                uint64_t pi_ = super_2_alphabet[propagated[j - 1]];
+                char pi = super_2_alphabet[propagated[j]];
+                char pi_ = super_2_alphabet[propagated[j - 1]];
                 // Compare pi and pi-1 with xor and mask the result to see only the last 3 bits
-                uint64_t compare_res = pi ^ pi_;
+                char compare_res = pi ^ pi_;
                 if (compare_res != 0) {
                     mismatch_found_marks[j] = 1;
                     lcs[j] = round + compare_2_char(compare_res);
@@ -255,7 +258,9 @@ sdsl::int_vector<> lcs_superalphabet_algorithm(const sbwt::plain_matrix_sbwt_t& 
         last = propagated;
     }
 
-    if (chars_per_super_alpha_char == 2) {return lcs;}
+    if (chars_per_super_alpha_char == 2) {
+        cerr << "Completed superalphabet-2"<< endl;
+        return lcs;}
 ////--------------------------------------------------- t = 4
     //Create a 4-superalphabet
     auto[super_4_alphabet, super_4_alphabet_map] = get_super_alphabet(super_2_alphabet, t/2);
@@ -264,12 +269,12 @@ sdsl::int_vector<> lcs_superalphabet_algorithm(const sbwt::plain_matrix_sbwt_t& 
     vector<int64_t> super_4_C_array(super_4_alphabet.size(), 0);
 
     for ( size_t i = 1; i < n_nodes; i++ ) { // start from 1 as later i-1 and $$ already ok
-        uint64_t pi = super_2_alphabet[propagated[i]];
-        uint64_t li = super_2_alphabet[last[i]];
-        uint64_t key = ( pi << (3*t/2)) | li;
+        char pi = super_2_alphabet[propagated[i]];
+        char li = super_2_alphabet[last[i]];
+        char key = ( pi << (3*t/2)) | li;
 
         if (mismatch_found_marks[i]){
-            uint64_t c = super_4_alphabet_map[key];
+            char c = super_4_alphabet_map[key];
             while(super_4_C_array[c]==0){
                 super_4_C_array[c]=i;
                 c--;
@@ -277,12 +282,12 @@ sdsl::int_vector<> lcs_superalphabet_algorithm(const sbwt::plain_matrix_sbwt_t& 
             // mismatch in the 3rd or 4th char
         } else if (!mismatch_found_marks[i]){
             // Compare pi and pi-1 with xor and mask the result to see only the last 3 bits
-            uint64_t pi_ = super_2_alphabet[propagated[i-1]];
-            uint64_t compare_res = pi ^ pi_;
+            char pi_ = super_2_alphabet[propagated[i-1]];
+            char compare_res = pi ^ pi_;
             if (compare_res != 0) {
                 mismatch_found_marks[i] = 1;
                 lcs[i] = 2 + compare_2_char(compare_res);
-                uint64_t c = super_4_alphabet_map[key];
+                char c = super_4_alphabet_map[key];
                 while(super_4_C_array[c]==0){
                     super_4_C_array[c]=i;
                     c--;
@@ -310,6 +315,7 @@ sdsl::int_vector<> lcs_superalphabet_algorithm(const sbwt::plain_matrix_sbwt_t& 
 ////     Propagate the labels one step forward in the graph
     for (uint64_t round = t; round < (k+t-1); round += t) { // works w chars_per_super_alpha_char = 2
         // Propagate the labels one step forward in the graph
+        cerr << "Round_4 " << to_string(round) << endl;
         vector <uint64_t> super_C_copy = {0};
         super_C_copy.insert(super_C_copy.end(), super_4_C_array.begin(), super_4_C_array.end());
         uint64_t subsets = 0;
@@ -328,14 +334,14 @@ sdsl::int_vector<> lcs_superalphabet_algorithm(const sbwt::plain_matrix_sbwt_t& 
 //            break;}
         for (uint64_t j = 1; j < n_nodes; j++) {
             if (!mismatch_found_marks[j]) {
-                uint64_t pi = super_4_alphabet[propagated[j]];
-                uint64_t pi_ = super_4_alphabet[propagated[j-1]];
+                char pi = super_4_alphabet[propagated[j]];
+                char pi_ = super_4_alphabet[propagated[j-1]];
                 // Compare pi and pi-1 with xor and mask the result to see only the last 3 bits
-                uint64_t compare_res = pi ^ pi_;
+                char compare_res = pi ^ pi_;
                 if (compare_res != 0) {
                     mismatch_found_marks[j] = 1;
                     // Check only c2 (last 6 bits)
-                    uint64_t compare_res_12 = compare_res & 0b111111;
+                    char compare_res_12 = compare_res & 0b111111;
                     if (compare_res_12 != 0) { // the last 6 bits are not the same
                         lcs[j] = round + compare_2_char(compare_res_12);
                     } else { // No mismatch in the last 6 bits -> mismatch in the previous
